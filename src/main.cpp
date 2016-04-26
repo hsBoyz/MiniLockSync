7377b62941a6ec63d3e565b81e8a982c3cc68aa9
hsBoyz <matuit00@hs-esslingen.de>
2016-04-26 15:17:10 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "stackedwindow.h"
+#include "steerer.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    Steerer *s = new Steerer();
+    s->start();
+
     w.show();
 
     return a.exec();
