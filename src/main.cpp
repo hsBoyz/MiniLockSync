62c349cd0edf488fc37911b433f236552c96a9da
hsBoyz <matuit00@hs-esslingen.de>
2016-05-07 15:43:58 +0200

    added several paths. login now working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "steerer.h"
+#include "login.hpp"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -7,11 +8,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
     MainWindow w;
+    login l;
+    l.show();
 
     Steerer *s = new Steerer();
     s->start();
 
-    w.show();
+    //w.show();
 
     return a.exec();
 }
