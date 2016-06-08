46578f81281c68e3e46ea89db388119d548eb0b3
LucaH <luca.hanf@gmail.com>
2016-06-08 14:54:54 +0200

    -

--- tools/ucryptlib/ucryptlibtest/ucryptlibtest/tests/TUnit_DecryptFile_Two_Recipients.h
+++
@@ -1,6 +0,0 @@
-#ifndef TUNIT_DECRYPTFILE_TWO_RECIPIENTS_H
-#define TUNIT_DECRYPTFILE_TWO_RECIPIENTS_H
-
-extern void TUnit_DecryptFile_Two_Recipients();
-
-#endif
--- tools/ucryptlib/ucryptlibtest/ucryptlibtest/tests/TUnit_Private_Key.h
+++
@@ -1,6 +0,0 @@
-#ifndef TUNIT_PRIVATE_KEY_H
-#define TUNIT_PRIVATE_KEY_H
-
-extern void TUnit_Private_Key();
-
-#endif
